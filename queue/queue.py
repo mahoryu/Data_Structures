205a64c4e80ef20e3304d2ca7eea9c33e7133ce0
Ethan Holden <mahoryu@gmail.com>
2020-07-22 21:43:45 -0600

    Passed the queue test using the LinkedList as the underlying storage

--- queue/queue.py
+++ queue/queue.py
@@ -14,23 +14,47 @@ Stretch: What if you could only use instances of your Stack class to implement t
          What would that look like? How many Stacks would you need? Try it!
 """
 # impliment using an array as the underlying storage:
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+#         self.size += 1
+
+#     def dequeue(self):
+#         if not self.storage:
+#             return
+#         else:
+#             value = self.storage[0]
+#             self.storage.pop(0)
+#             self.size -= 1
+#             return value
+
+# impliment using a LinkedList as the underlying storage:
+from singly_linked_list import LinkedList
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
         self.size += 1
 
     def dequeue(self):
-        if not self.storage:
+        if not self.storage.head:
             return
         else:
-            value = self.storage[0]
-            self.storage.pop(0)
+            value = self.storage.head.get_value()
+            self.storage.remove_head()
             self.size -= 1
             return value
