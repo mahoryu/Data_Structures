6b6507b99aa5a09cb1db31c3c5453e0b42d68aa1
Ethan Holden <mahoryu@gmail.com>
2020-07-22 21:38:44 -0600

    Passed the Queue test with the array as the underlying storage

--- queue/queue.py
+++ queue/queue.py
@@ -9,20 +9,28 @@ return elements in First In First Out order.
    Make sure the Queue tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
-   
+
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+# impliment using an array as the underlying storage:
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = []
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if not self.storage:
+            return
+        else:
+            value = self.storage[0]
+            self.storage.pop(0)
+            self.size -= 1
+            return value
